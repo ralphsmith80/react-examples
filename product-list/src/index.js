9bb3b757634ec7f84773a8369522280b2c834361
Ralph Smith <agentsmith@us.ibm.com>
2016-08-03 09:15:44 -0600

    add a product-list example

---
+++ product-list/src/index.js
@@ -0,0 +1,20 @@
+// This example is from thinking-in-react docs
+// https://facebook.github.io/react/docs/thinking-in-react.html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FilterableProductTable from './App';
+import './index.css';
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('root')
+);
